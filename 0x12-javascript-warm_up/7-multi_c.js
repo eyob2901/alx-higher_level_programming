4ae7cb557f6db2263d254aab19eb86de46e13a84
Eyob Eshetu <eyob2901@gmail.com>
2022-12-01 12:09:42 -0800

    delete

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = require('process');
-const num = parseInt(argv[2]);
-const printC = (cuantity) => {
-  for (; cuantity > 0; cuantity--) console.log('C is fun');
-};
-
-Number.isInteger(num) ? printC(num) : console.log('Missing number of occurrences');
