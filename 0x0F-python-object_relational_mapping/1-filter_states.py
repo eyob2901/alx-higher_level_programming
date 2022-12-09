73cdde3372f73edefd9640b65dc24242ab8b3023
eyob2901 <104759138+eyob2901@users.noreply.github.com>
2022-12-10 00:21:24 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import sys
-import MySQLdb
+"""Lists all the states with a name starting with N
+    from database hbtn_0e_0_usa.
+"""
 
-if __name__ == "__main__":
-    """ MAIN FUNCTION"""
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    row = cur.fetchall()
+    for state in row:
+        print(state)
+    cur.close()
+    db.close()
