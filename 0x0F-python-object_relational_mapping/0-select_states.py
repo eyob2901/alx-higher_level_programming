84141a0309922178f23f651d921241559f55ed10
eyob2901 <104759138+eyob2901@users.noreply.github.com>
2022-12-10 00:08:42 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     """MAIN FUNCTION"""
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
+                         port=3300,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
