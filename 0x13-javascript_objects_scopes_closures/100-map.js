ba6446d4b91b7c751c364ad71b71e2d417f5e070
Eyob Eshetu <eyob2901@gmail.com>
2022-12-13 12:16:23 -0800

    javascript object

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-// const newlist = [];
-
-const newlist = list.map((num, index) => {
-  // newlist.push(num * index);
-  return (num * index);
-});
 console.log(list);
-console.log(newlist);
+console.log(list.map((item, index) => item * index));
