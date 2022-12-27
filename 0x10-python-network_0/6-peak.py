9198cd8819477d94699df96cab32405fa7d26367
Eyob Eshetu <eyob2901@gmail.com>
2022-12-27 11:36:25 -0800

    pynet

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,7 @@
 
 
 def find_peak(list_of_integers):
-        """Finds a peak in a list of unsorted integers."""
-            if list_of_integers:
-                        list_of_integers.sort(reverse=True)
-                                return list_of_integers[0]
+            """Finds a peak in a list of unsorted integers."""
+                if list_of_integers:
+                                list_of_integers.sort(reverse=True)
+                                        return list_of_integers[0]
